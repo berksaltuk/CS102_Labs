52a7816ca000d2867f3f0fd8be3528deee7a9eb1
Berk Saltuk Yılmaz <61010486+berksaltuk@users.noreply.github.com>
2022-02-12 16:42:42 +0300

    Delete Lab03/part a directory

--- Lab03/part a/src/Locatable.java
+++
@@ -1,7 +0,0 @@
-public interface Locatable {
-   
-   public int getX();
-   public int getY();
-   public void setLocation( int x, int y);
-
-}
--- Lab03/part a/src/Selectable.java
+++
@@ -1,7 +0,0 @@
-public interface Selectable{
-
-   boolean getSelected();
-   void setSelected( boolean selected);
-   Shape contains( int x, int y);
-      
-}
